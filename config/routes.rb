388d8b9e61932545e9c4d9f0f50917dc81ded2e8
karthi07 <itkarthi02@gmail.com>
2020-06-08 20:45:52 +0530

    add devise and create User model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :reviews
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'reviews#index'
 end
