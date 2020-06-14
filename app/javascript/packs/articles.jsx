01521f7f539ede701a3c7be3d6f9dbbadd3bd0dd
karthi07 <itkarthi02@gmail.com>
2020-06-14 14:20:45 +0530

    setup react

---
+++ app/javascript/packs/articles.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<App />, document.getElementById("react-div"));
+});
