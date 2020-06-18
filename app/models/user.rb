85a25fd0ccc8d8b2f1827563e83d2ae8e7f52ffc
karthi07 <itkarthi02@gmail.com>
2020-06-18 22:22:48 +0530

    final changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { in: 4..20 }
   validates :fullname, presence: true, length: { in: 4..40 }
   
-  has_many :reviews
-  has_many :followers, :class_name => "Following", :foreign_key => "followed_id"
-  has_many :following, :class_name => "Following", :foreign_key => "follower_id"
+  has_many :reviews, dependent: :destroy
+  has_many :followers, :class_name => "Following", :foreign_key => "followed_id", dependent: :destroy
+  has_many :following, :class_name => "Following", :foreign_key => "follower_id", dependent: :destroy
   has_many :followers_list, through: :followers, source: :follower
   has_many :following_list, through: :following, source: :followed
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{ fullname: 'Test User 1', username: 'testuser1', email: 'test1@test.com', password: '123456', password_confirmation: '123456' }, { fullname: 'Test User 2', username: 'testuser2', email: 'test2@test.com', password: '123456', password_confirmation: '123456' }, { fullname: 'Test User 3', username: 'testuser3', email: 'test3@test.com', password: '123456', password_confirmation: '123456' }, { fullname: 'Test User 4', username: 'testuser4', email: 'test4@test.com', password: '123456', password_confirmation: '123456' },{ fullname: 'Test User 5', username: 'testuser5', email: 'test5@test.com', password: '123456', password_confirmation: '123456' } ])
